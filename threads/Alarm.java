fcd626bf5599de3eb04ae8ee2451af4184d5238e
ubushin <ottri1998@gmail.com>
2020-11-08 21:48:17 +0600

    part1;task3;alarm done

--- threads/Alarm.java
+++ threads/Alarm.java
@@ -1,5 +1,8 @@
 package nachos.threads;
 
+import java.util.LinkedList; 
+import java.util.Queue;
+
 import nachos.machine.*;
 
 /**
@@ -23,11 +26,29 @@ public class Alarm {
     /**
      * The timer interrupt handler. This is called by the machine's timer
      * periodically (approximately every 500 clock ticks). Causes the current
-     * thread to yield, forcing a context switch if there is another thread
+     * thread to yield, forcing a context switch if there is another threadCauses the current
+     * thread to yield, forcing a context switch if there is
      * that should be run.
      */
     public void timerInterrupt() {
-	KThread.currentThread().yield();
+        boolean s = Machine.interrupt().disable();
+        long current_time = Machine.timer().getTime();
+        int len = sleepingQueue.size();
+        
+        for(int i=0; i<len; i++)
+        {
+            Pair p = sleepingQueue.remove();
+            if(p.time < current_time)
+            {
+                p.t.ready();
+            }
+            else
+            {
+                sleepingQueue.add(p);
+            }
+        }
+        KThread.currentThread().yield();
+        Machine.interrupt().restore(s);
     }
 
     /**
@@ -46,8 +67,35 @@ public class Alarm {
      */
     public void waitUntil(long x) {
 	// for now, cheat just to get something working (busy waiting is bad)
-	long wakeTime = Machine.timer().getTime() + x;
-	while (wakeTime > Machine.timer().getTime())
-	    KThread.yield();
+        long wakeTime = Machine.timer().getTime() + x;
+        KThread ct = KThread.currentThread();
+        
+        if(ct != null)
+        {
+            boolean s = Machine.interrupt().disable();
+            Pair newEntry = new Pair();
+            newEntry.t = ct;
+            newEntry.time = wakeTime;
+            sleepingQueue.add(newEntry);
+            KThread.sleep();
+            Machine.interrupt().restore(s);
+        }
+       
+	/*while (wakeTime > Machine.timer().getTime())
+	    KThread.yield();*/
     }
+    
+    private class Pair {
+        public KThread t;
+        public long time;
+        
+        /*public int compareTo(Pair p2)
+        {
+            if(time > p2.time) return 1;
+            else if(time < p2.time) return -1;
+            else return 0;
+        }*/
+    }
+    
+    Queue<Pair> sleepingQueue = new LinkedList<>();
 }
